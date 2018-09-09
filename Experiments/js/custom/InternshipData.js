f2be0448ff87f6ed74a8d1efe7694763175ee0d0
Soph <sophiewarnes@gmail.com>
2018-09-09 17:16:31 +0100

    Add files via upload

---
+++ Experiments/js/custom/InternshipData.js
@@ -0,0 +1,58 @@
+$(function () {
+    $('#internships').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'London Internship Salaries'
+        },
+        subtitle: {
+            text:'Salaries for internships at national outlets in London'
+        },
+        xAxis: {
+            categories: ['The Economist Internship Schemes (JA)','ESPN Editorial Internship (JA)','The Telegraph Magazine Internship (JA)','The Financial Times (SB)','Time Inc Internship','Longitude Editorial Intern (SB)','Internship, National Tabloid']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Salary (£)'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -30,
+            verticalAlign: 'top',
+            y: 50,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br />',
+            pointFormat: '{series.name}: {point.y}<br/><b>Total:</b> {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: false,
+                }
+            }
+        },
+        series: [{
+            name: 'Internships',
+            data: [24000,18000,18460,18564,15000,15000,14000],
+            color: '#63c6ae'
+
+        }]
+    });
+});
